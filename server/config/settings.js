a96e48257d821f21c962fb28797a7208fae92979
pkindalov <panayotkindalov@gmail.com>
2018-06-14 12:41:06 +0300

    changed name of the database. LiChanges in main view. Added only header and navigation. Changes in index view - added body of the main template file.Little change in register view. Added container-fluid div

--- server/config/settings.js
+++ server/config/settings.js
@@ -5,7 +5,7 @@ let rootPath = path.normalize(path.join(__dirname, '/../../'))
 module.exports = {
   development: {
     rootPath: rootPath,
-    db: 'mongodb://localhost:27017/yourDatabase',
+    db: 'mongodb://localhost:27017/carRentingNew',
     port: 1337
   },
   staging: {
