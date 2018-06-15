62d84d5512faf16d40507fd5215f784f38c49133
pkindalov <panayotkindalov@gmail.com>
2018-06-15 11:06:51 +0300

    Added model for a car

---
+++ server/data/Car.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const REQUIRED_VALIDATION_MESSAGE = '{PATH} is required'
+
+let carSchema = new mongoose.Schema({
+  model: {type: String, required: REQUIRED_VALIDATION_MESSAGE},
+  image: {type: String, required: REQUIRED_VALIDATION_MESSAGE},
+  pricePerDay: {type: Number, required: REQUIRED_VALIDATION_MESSAGE},
+  rentedDays: {type: Number, default: 0},
+  note: {type: String, required: REQUIRED_VALIDATION_MESSAGE},
+  isCarRented: {type: Boolean, required: REQUIRED_VALIDATION_MESSAGE},
+  dateOfPublication: {type: Date, default: new Date()},
+  addedBy: [{type: ObjectId, ref: 'User'}]
+})
+
+let Car = mongoose.model('Car', carSchema)
+
+module.exports = Car
